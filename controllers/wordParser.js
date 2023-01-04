48de3d7e06cb204306d01baf3de5567eaea85092
vlad <vlad.bras02@gmail.com>
2023-01-04 16:46:09 +0100

    fixed parser

--- controllers/wordParser.js
+++ controllers/wordParser.js
@@ -4,10 +4,8 @@ require('chromedriver')
 module.exports = wordParse = async(word) =>{
     let data = {}
     let driver = await new Builder().forBrowser("chrome").build()
-    let URL ='https://englishlib.org/dictionary/'
+    let URL =`https://englishlib.org/dictionary/en-ru/${word}.html`
     await driver.get(URL)
-    await driver.findElement(By.xpath('//*[@id="container"]/div[1]/div/div[1]/input')).sendKeys(word, Key.RETURN)
-    await driver.findElement(By.xpath('//*[@id="container"]/div[1]/div/div[1]/i')).click()
     data.ru = await driver.findElement(By.xpath('//*[@id="block_tr"]/div/i')).getText()
     data.transcription = await driver.findElement(By.xpath('//*[@id="us_tr_sound"]/span[1]/big')).getText()
     data.audio = await driver.findElement(By.xpath('//*[@id="audio_us"]/source')).getAttribute('src')
