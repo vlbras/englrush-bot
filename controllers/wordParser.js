a2b0c46dc1dfd0e30d67056777e71addbdee4eff
vlad <vlad.bras02@gmail.com>
2023-01-05 11:36:12 +0100

    axios added, puppeteer removed

--- controllers/wordParser.js
+++ controllers/wordParser.js
@@ -1,22 +1,20 @@
-const puppeteer = require('puppeteer')
+const axios = require('axios')
 const cheerio = require('cheerio')
 
 module.exports = start = async (word) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
     const url = `https://englishlib.org/dictionary/en-ru/${word}.html`
-
-    await page.goto(url)
-    const content = await page.content();
-    const $ = cheerio.load(content);
     const data = {}
-    data.ru = $('#block_tr > div > i').text()
-    data.transcription = $('#us_tr_sound > span.transcription > big').text()
-    data.audio = $('#audio_us > source').attr('src')
-    $('#content-tab6 > b').remove()
-    data.description = $('#content-tab6:first').text()
-    data.description = data.description.substring(12, data.description.length - 6)
-    console.log(data)
-    await browser.close()
+    await axios.get(url)
+        .then(res => {
+            const page = res.data
+            const $ = cheerio.load(page)
+            data.ru = $('#block_tr > div > i').text()
+            data.transcription = $('#us_tr_sound > span.transcription > big').text()
+            data.audio = $('#audio_us > source').attr('src')
+            $('#content-tab6 > b').remove()
+            data.description = $('#content-tab6:first').text()
+            data.description = data.description.substring(12, data.description.length - 6)
+            console.log(data)
+        })
     return data
 }
