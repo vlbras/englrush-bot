96468572dddeea0d9f7e23cb6603d21171f4463c
vlad <vlad.bras02@gmail.com>
2023-02-11 22:58:08 +0100

    The best code refactoring for Quizzes ever

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -9,70 +9,61 @@ class QuizController {
     async word(chatId, n, _id) {
         if (!_id) return topicOptions(chatId, `quizword 0 &&`)
 
-        if (!await Topic.findById(_id)) return bot.sendMessage(chatId, `❗️Please select a topic`)
+        let ruAnswers = []
+        let enQuestions = []
         let words = await Word.find({ topicId: _id })
-        if (!words.length) return bot.sendMessage(chatId, `❗️No word added.\n${wordCommand}`, { parse_mode: "HTML" })
-        if (words.length < 5) return bot.sendMessage(chatId, `❗️Too few words. 5 is minimum`)
+        words.forEach(el => {
+            ruAnswers.push(el.ru) // anyAnswers
+            enQuestions.push(el.en) // anyQuestions
+        })
 
-        n = Number(n)
-        let i = words.length - 1 - n
-        let last = i - 5
-        for (i; i > last; i--) {
-            if (!words[i]) return;
-            let answers = [words[i].ru]
-            while (answers.length != 5) {
-                let rand = Math.floor(Math.random() * (words.length))
-                if (!answers.includes(words[rand].ru)) answers.push(words[rand].ru)
-                let temp = answers[rand % answers.length]
-                answers[rand % answers.length] = answers[i % answers.length]
-                answers[i % answers.length] = temp
-            }
-            await bot.sendPoll(chatId, `${words[i].en}`, [answers[0], answers[1], answers[2], answers[3], answers[4]], { type: 'quiz', correct_option_id: answers.indexOf(words[i].ru) })
-            answers = []
-        }
-        if (words[i]) {
-            return bot.sendMessage(chatId, `Would you like to continue?`, {
-                reply_markup: JSON.stringify({
-                    inline_keyboard: [[{ text: 'Next', callback_data: `quizword ${words.length - 1 - i} && ${_id}` }]]
-                })
-            })
-        }
-        return
+        return startQuiz(chatId, n, _id, words, ruAnswers, enQuestions)
     }
 
     async context(chatId, n, _id) {
         if (!_id) return topicOptions(chatId, `quizcontext 0 &&`)
 
-        if (!await Topic.findById(_id)) return bot.sendMessage(chatId, `❗️Please select a topic`)
+        let enAnswers = []
+        let enContext = []
         let words = await Word.find({ topicId: _id })
-        if (!words.length) return bot.sendMessage(chatId, `❗️No word added.\n${wordCommand}`, { parse_mode: "HTML" })
-        if (words.length < 5) return bot.sendMessage(chatId, `❗️Too few words. 5 is minimum`)
-
-        n = Number(n)
-        let i = words.length - 1 - n
-        let last = i - 5
-        for (i; i > last; i--) {
-            if (!words[i]) return;
-            let answers = [words[i].en]
-            while (answers.length != 5) {
-                let rand = Math.floor(Math.random() * (words.length))
-                if (!answers.includes(words[rand].en)) answers.push(words[rand].en)
-                let temp = answers[rand % answers.length]
-                answers[rand % answers.length] = answers[i % answers.length]
-                answers[i % answers.length] = temp
-            }
-            await bot.sendPoll(chatId, `${words[i].context[0].en}`, [answers[0], answers[1], answers[2], answers[3], answers[4]], { type: 'quiz', correct_option_id: answers.indexOf(words[i].en) })
-            answers = []
+        let quizNumber = 0
+        for (let i = 0; i < words.length; i++) {
+            enAnswers.push(words[i].en)
+            enContext.push(words[i].context[quizNumber].en)
         }
-        if (words[i]) {
-            return bot.sendMessage(chatId, `Would you like to continue?`, {
-                reply_markup: JSON.stringify({
-                    inline_keyboard: [[{ text: 'Next', callback_data: `quizcontext ${words.length - 1 - i} && ${_id}` }]]
-                })
-            })
-        }
-        return
+            return startQuiz(chatId, n, _id, words, enAnswers, enContext)
     }
+}
+
+let startQuiz = async (chatId, n, _id, words, anyAnswers, anyQuestions) => {
+    if (!await Topic.findById(_id)) return bot.sendMessage(chatId, `❗️Please select a topic`)
+    if (!words.length) return bot.sendMessage(chatId, `❗️No word added.\n${wordCommand}`, { parse_mode: "HTML" })
+    if (words.length < 5) return bot.sendMessage(chatId, `❗️Too few words. 5 is minimum`)
+
+    n = Number(n)
+    let i = words.length - 1 - n
+    let last = i - 5
+    for (i; i > last; i--) {
+        if (!words[i]) return;
+        let answers = [anyAnswers[i]] // anyAnswers[i]
+        while (answers.length != 5) {
+            let rand = Math.floor(Math.random() * (words.length))
+            if (!answers.includes(anyAnswers[rand])) answers.push(anyAnswers[rand]) // anyAnswers[rand]
+            let temp = answers[rand % answers.length]
+            answers[rand % answers.length] = answers[i % answers.length]
+            answers[i % answers.length] = temp
+        }
+        await bot.sendPoll(chatId, `${anyQuestions[i]}`, [answers[0], answers[1], answers[2], answers[3], answers[4]], { type: 'quiz', correct_option_id: answers.indexOf(anyAnswers[i]) })
+        answers = []
+    }
+    if (words[i]) {
+        return bot.sendMessage(chatId, `Would you like to continue?`, {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [[{ text: 'Next', callback_data: `quizword ${words.length - 1 - i} && ${_id}` }]]
+            })
+        })
+    }
+    return
 }
 
 module.exports = new QuizController()
