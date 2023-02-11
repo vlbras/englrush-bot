a66202c5e46f237e5cbb5338e7fab80876a182b7
vlad <vlad.bras02@gmail.com>
2023-02-11 20:46:59 +0100

    Context Quiz added

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -40,8 +40,38 @@ class QuizController {
         return
     }
 
-    async context(chatId, i, _id) {
+    async context(chatId, n, _id) {
+        if (!_id) return topicOptions(chatId, `quizcontext 0 &&`)
 
+        if (!await Topic.findById(_id)) return bot.sendMessage(chatId, `❗️Please select a topic`)
+        let words = await Word.find({ topicId: _id })
+        if (!words.length) return bot.sendMessage(chatId, `❗️No word added.\n${wordCommand}`, { parse_mode: "HTML" })
+        if (words.length < 5) return bot.sendMessage(chatId, `❗️Too few words. 5 is minimum`)
+
+        n = Number(n)
+        let i = words.length - 1 - n
+        let last = i - 5
+        for (i; i > last; i--) {
+            if (!words[i]) return;
+            let answers = [words[i].en]
+            while (answers.length != 5) {
+                let rand = Math.floor(Math.random() * (words.length))
+                if (!answers.includes(words[rand].en)) answers.push(words[rand].en)
+                let temp = answers[rand % answers.length]
+                answers[rand % answers.length] = answers[i % answers.length]
+                answers[i % answers.length] = temp
+            }
+            await bot.sendPoll(chatId, `${words[i].context[0].en}`, [answers[0], answers[1], answers[2], answers[3], answers[4]], { type: 'quiz', correct_option_id: answers.indexOf(words[i].en) })
+            answers = []
+        }
+        if (words[i]) {
+            return bot.sendMessage(chatId, `Would you like to continue?`, {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: [[{ text: 'Next', callback_data: `quizcontext ${words.length - 1 - i} && ${_id}` }]]
+                })
+            })
+        }
+        return
     }
 }
 
