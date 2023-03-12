41fb218ca487c18c8b18255ea268adf668395406
vlad <vlad.bras02@gmail.com>
2023-03-12 11:15:57 +0100

    ucFirst in adding context

--- models/models.js
+++ models/models.js
@@ -57,12 +57,30 @@ const wordSchema = new Schema({
     },
 })
 
+const dictionarySchema = new Schema({
+    en: {
+        type: String,
+        require: true
+    },
+    uk: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    context: [String]
+})
+
 const Folder = mongoose.model('Folder', folderSchema)
 const Topic = mongoose.model('Topic', topicSchema)
 const Word = mongoose.model('Word', wordSchema)
+const Dictionary = mongoose.model('Dictionary', dictionarySchema)
 
 module.exports = {
     Folder,
     Topic,
-    Word
+    Word,
+    Dictionary
 }
